f21fc9e5bfe831a06fa9ecc74c7a2c33e3b4d6f9
Nayla Salwa <naylasalwahayati@gmail.com>
2026-02-21 20:15:13 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pertemuan 1</title>
+</head>
+<body style="text-align:center; margin-top:200px; font-family:Arial;">
+    <h1>Nama : Nayla Salwa</h1>
+    <h2>NIM : 20230140218</h2>
+    <h3>Praktikum Laravel Pertemuan 1</h3>
+</body>
+</html>
